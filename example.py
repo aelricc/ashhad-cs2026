4c0806707b1797ed2da3649c7d5f0a92896aaeb8
ashhad <125950620+aelricc@users.noreply.github.com>
2026-03-05 16:26:12 -0500

    Delete example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-#import idcard.py
-
-num = 2
-#This is my cool program
-answer = input("What is 1 + 1: ")
-if(int(answer) == 2):
-    print("Correct! Good job.")
-else:
-    print("Incorrect. Try again!")
